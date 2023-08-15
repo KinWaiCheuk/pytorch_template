b5c7da67a8d39293d7f706cca72fe2297b351f1f
KinWaiCheuk <u3500684@connect.hku.hk>
2023-08-15 21:00:22 +0900

    added working model: Global Token (Reduced_CNN)

--- models/Models.py
+++ models/Models.py
@@ -524,6 +524,343 @@ class AvgPool_CNN_Early(AMT):
         return output
     
 
+class AvgPool_CNN_Early_Vanilla(AMT):
+    def __init__(self,
+                 spec_layer,
+                 norm_mode,
+                 input_dim,
+                 onset=False,
+                 hidden_dim=768,
+                 output_dim=88,
+                 mfm=False,
+                 task_kargs=None):
+        super().__init__(**task_kargs)
+        self.save_hyperparameters(ignore=['spec_layer'])
+        
+        # input_dim = 229
+        # which is the spectrogram bins
+        self.input_dim = input_dim
+        self.spec_layer = spec_layer
+        self.mfm = mfm
+
+        if self.mfm:
+            if self.mfm == 'top':
+                self.mfm_dim = 3600
+            elif self.mfm == 'middle' or self.mfm == 'bottom':
+                self.mfm_dim = 3200            
+            # 1frame = 8 token
+            # self.mfm_proj = nn.Linear(3600, input_dim)
+            self.avg_pool = torch.nn.AvgPool1d(8)
+        else:
+            self.mfm_dim = 3600 # hard encoding for quick test
+
+
+
+        self.norm_layer = Normalization(mode=norm_mode)
+        self.onset = onset
+        
+        self.cnn = nn.Sequential(
+            # layer 0
+            nn.Conv2d(1, hidden_dim // 16, (3, 3), padding=1),
+            nn.BatchNorm2d(hidden_dim // 16),
+            nn.ReLU(),
+            # layer 1
+            nn.Conv2d(hidden_dim // 16, hidden_dim // 16, (3, 3), padding=1),
+            nn.BatchNorm2d(hidden_dim // 16),
+            nn.ReLU(),
+            # layer 2
+            nn.MaxPool2d((1, 2)),
+            nn.Dropout(0.25),
+            nn.Conv2d(hidden_dim // 16, hidden_dim // 8, (3, 3), padding=1),
+            nn.BatchNorm2d(hidden_dim // 8),
+            nn.ReLU(),
+            # layer 3
+            nn.MaxPool2d((1, 2)),
+            nn.Dropout(0.25),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear((hidden_dim // 8) * ((input_dim+self.mfm_dim) // 4), hidden_dim),
+            nn.Dropout(0.5)
+        )
+        
+        self.frame_classifier = nn.Linear(hidden_dim, output_dim)
+        if self.onset:
+            self.onset_classifier = nn.Linear(hidden_dim, output_dim)
+        
+    def forward(self, x, mfm_tokens=None):
+        # if x2 is given, do cross attention
+        spec = self.spec_layer(x) # (B, F, T)
+        spec = torch.log(spec+1e-8)
+        spec = spec.transpose(1,2) # (B, T, F)
+
+        spec = self.norm_layer(spec)
+
+
+        if mfm_tokens != None:
+            # sanity check to avoid bugs
+            assert self.mfm, "mfm_tokens is given but mfm is not set to True"
+            # mfm_tokens = self.mfm_proj(mfm_tokens)
+            # downsample mfm_tokens to be the same size as spec
+            mfm_tokens = self.avg_pool(mfm_tokens.transpose(-1,-2)).transpose(-1,-2)
+            #(B, T, F)
+
+            # discard the last frame of spec
+            # because we don't have enough mfm_tokens for the last frame
+        else:
+            mfm_tokens = torch.randn(spec.shape[0], spec.shape[1]-1, self.mfm_dim).to(spec.device)
+        x = torch.cat([spec[:,:-1], mfm_tokens], dim=-1)            
+        # combine spec and mfm_tokens
+
+        x = self.cnn(x.unsqueeze(1)) # (B, hidden_dim//8, T, F//4)
+        x = x.transpose(1,2).flatten(2)
+        x = self.fc(x) # (B, T, hidden_dim//8*F//4)
+        
+        pred_frame = self.frame_classifier(x)
+        if self.onset:
+            pred_onset = self.onset_classifier(x)
+
+            output = {"frame": pred_frame,
+                      "onset": pred_onset,
+                      "spec": spec}
+        else:
+            output = {"frame": pred_frame,
+                      "spec": spec}
+            
+        return output
+    
+
+class AvgPool_CNN_Late_Vanilla(AMT):
+    def __init__(self,
+                 spec_layer,
+                 norm_mode,
+                 input_dim,
+                 onset=False,
+                 hidden_dim=768,
+                 output_dim=88,
+                 mfm=False,
+                 task_kargs=None):
+        super().__init__(**task_kargs)
+        self.save_hyperparameters(ignore=['spec_layer'])
+        
+        # input_dim = 229
+        # which is the spectrogram bins
+        self.input_dim = input_dim
+        self.spec_layer = spec_layer
+        self.mfm = mfm
+
+        if self.mfm:
+            if self.mfm == 'top':
+                self.mfm_dim = 3600
+            elif self.mfm == 'middle' or self.mfm == 'bottom':
+                self.mfm_dim = 3200            
+            # 1frame = 8 token
+            # self.mfm_proj = nn.Linear(3600, input_dim)
+            self.avg_pool = torch.nn.AvgPool1d(8)            
+        else:
+            self.mfm_dim = 3600 # hard encoding for quick test
+        self.norm_layer = Normalization(mode=norm_mode)
+        self.onset = onset
+
+        
+        self.cnn = nn.Sequential(
+            # layer 0
+            nn.Conv2d(1, hidden_dim // 16, (3, 3), padding=1),
+            nn.BatchNorm2d(hidden_dim // 16),
+            nn.ReLU(),
+            # layer 1
+            nn.Conv2d(hidden_dim // 16, hidden_dim // 16, (3, 3), padding=1),
+            nn.BatchNorm2d(hidden_dim // 16),
+            nn.ReLU(),
+            # layer 2
+            nn.MaxPool2d((1, 2)),
+            nn.Dropout(0.25),
+            nn.Conv2d(hidden_dim // 16, hidden_dim // 8, (3, 3), padding=1),
+            nn.BatchNorm2d(hidden_dim // 8),
+            nn.ReLU(),
+            # layer 3
+            nn.MaxPool2d((1, 2)),
+            nn.Dropout(0.25),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear((hidden_dim // 8) * ((input_dim) // 4), hidden_dim),
+            nn.Dropout(0.5)
+        )
+        
+        self.frame_classifier = nn.Linear(hidden_dim+self.mfm_dim, output_dim)
+        if self.onset:
+            self.onset_classifier = nn.Linear(hidden_dim*2, output_dim)
+        
+    def forward(self, x, mfm_tokens=None):
+        # if x2 is given, do cross attention
+        spec = self.spec_layer(x) # (B, F, T)
+        spec = torch.log(spec+1e-8)
+        spec = spec.transpose(1,2) # (B, T, F)
+
+        spec = self.norm_layer(spec)
+
+
+        if mfm_tokens != None:
+            # sanity check to avoid bugs
+            assert self.mfm, "mfm_tokens is given but mfm is not set to True"
+            # mfm_tokens = self.mfm_proj(mfm_tokens)
+            # downsample mfm_tokens to be the same size as spec
+            mfm_tokens = self.avg_pool(mfm_tokens.transpose(-1,-2)).transpose(-1,-2)
+            #(B, T, F)
+
+            # discard the last frame of spec
+            # because we don't have enough mfm_tokens for the last frame
+        else:
+            mfm_tokens = torch.randn(spec.shape[0], spec.shape[1]-1, self.mfm_dim).to(spec.device)
+        # combine spec and mfm_tokens
+
+        x = self.cnn(spec.unsqueeze(1)) # (B, hidden_dim//8, T, F//4)
+        x = x.transpose(1,2).flatten(2)
+        
+        x = self.fc(x) # (B, T, hidden_dim//8*F//4)
+        x = torch.cat([x[:,:-1], mfm_tokens], dim=-1)
+        
+        pred_frame = self.frame_classifier(x)
+        if self.onset:
+            pred_onset = self.onset_classifier(x)
+
+            output = {"frame": pred_frame,
+                      "onset": pred_onset,
+                      "spec": spec}
+        else:
+            output = {"frame": pred_frame,
+                      "spec": spec}
+            
+        return output
+    
+class Reduced_CNN_Late(AMT):
+    def __init__(self,
+                 spec_layer,
+                 norm_mode,
+                 input_dim,
+                 onset=False,
+                 hidden_dim=768,
+                 output_dim=88,
+                 mfm=False,
+                 task_kargs=None):
+        super().__init__(**task_kargs)
+        self.save_hyperparameters(ignore=['spec_layer'])
+        
+        # input_dim = 229
+        # which is the spectrogram bins
+        self.input_dim = input_dim
+        self.spec_layer = spec_layer
+        self.mfm = mfm
+
+        if self.mfm:
+            if self.mfm == 'top':
+                self.mfm_dim = 3600
+            elif self.mfm == 'middle' or self.mfm == 'bottom':
+                self.mfm_dim = 3200            
+            # 1frame = 8 token
+            # self.mfm_proj = nn.Linear(3600, input_dim)
+            self.avg_pool = torch.nn.AvgPool1d(8)            
+        else:
+            self.mfm_dim = 3600 # hard encoding for quick test
+        self.norm_layer = Normalization(mode=norm_mode)
+        self.onset = onset
+
+        
+        self.cnn = nn.Sequential(
+            # layer 0
+            nn.Conv2d(1, hidden_dim // 16, (3, 3), padding=1),
+            nn.BatchNorm2d(hidden_dim // 16),
+            nn.ReLU(),
+            # layer 1
+            nn.Conv2d(hidden_dim // 16, hidden_dim // 16, (3, 3), padding=1),
+            nn.BatchNorm2d(hidden_dim // 16),
+            nn.ReLU(),
+            # layer 2
+            nn.MaxPool2d((1, 2)),
+            nn.Dropout(0.25),
+            nn.Conv2d(hidden_dim // 16, hidden_dim // 8, (3, 3), padding=1),
+            nn.BatchNorm2d(hidden_dim // 8),
+            nn.ReLU(),
+            # layer 3
+            nn.MaxPool2d((1, 2)),
+            nn.Dropout(0.25),
+        )
+
+        self.token_cnn = torch.nn.Sequential(
+            # layer 0
+            # layer 0
+            nn.Conv2d(1, 16, (7, 11), padding=0, stride=1, dilation=2),    
+            nn.MaxPool2d((8,1)),
+            nn.ReLU(),
+            # layer 1
+            nn.Conv2d(16, 16, (5, 7), padding=0, stride=3, dilation=1),
+            nn.AvgPool2d((4,2)),
+            nn.ReLU(),
+            # layer 2
+            nn.Conv2d(16,32, (5, 5), padding=0, stride=2, dilation=1),
+            nn.AvgPool2d((2,4)),
+            nn.ReLU(),
+
+            # layer 2
+            nn.Conv2d(32,64, (5, 5), padding=0, stride=1, dilation=1),
+            nn.AvgPool2d((1,8)),
+            nn.ReLU(),    
+            # layer 3
+            # nn.Dropout(0.25),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear((hidden_dim // 8) * ((input_dim) // 4), hidden_dim),
+            nn.Dropout(0.5)
+        )
+        
+        self.frame_classifier = nn.Linear(hidden_dim+512, output_dim)
+        if self.onset:
+            self.onset_classifier = nn.Linear(hidden_dim*2, output_dim)
+        
+    def forward(self, x, mfm_tokens=None):
+        # if x2 is given, do cross attention
+        spec = self.spec_layer(x) # (B, F, T)
+        spec = torch.log(spec+1e-8)
+        spec = spec.transpose(1,2) # (B, T, F)
+
+        spec = self.norm_layer(spec)
+
+
+        x = self.cnn(spec.unsqueeze(1)) # (B, hidden_dim//8, T, F//4)
+        x = x.transpose(1,2).flatten(2) # (B, T, hidden_dim//8*F//4)
+        
+        x = self.fc(x) 
+
+        if mfm_tokens != None:
+            # sanity check to avoid bugs
+            assert self.mfm, "mfm_tokens is given but mfm is not set to True"
+            # mfm_tokens = self.mfm_proj(mfm_tokens)
+            # downsample mfm_tokens to be the same size as spec
+            mfm_tokens = self.token_cnn(mfm_tokens.unsqueeze(1))
+            #(B, T, F)
+
+            # discard the last frame of spec
+            # because we don't have enough mfm_tokens for the last frame
+        else:
+            mfm_tokens = torch.randn(spec.shape[0], spec.shape[1]-1, self.mfm_dim).to(spec.device)
+        # combine spec and mfm_tokens
+        mfm_flatten = mfm_tokens.flatten(1).unsqueeze(1)
+        x = torch.cat([x[:,:-1], mfm_flatten.repeat(1,320,1)], dim=-1) # hard coding the feature
+        
+        pred_frame = self.frame_classifier(x)
+        if self.onset:
+            pred_onset = self.onset_classifier(x)
+
+            output = {"frame": pred_frame,
+                      "onset": pred_onset,
+                      "spec": spec}
+        else:
+            output = {"frame": pred_frame,
+                      "spec": spec}
+            
+        return output        
+    
+
 class AvgPool_CNN_Early_proj(AMT):
     def __init__(self,
                  spec_layer,
